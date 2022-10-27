878069b76e5830e3296515a416b1cdd1d7eca1aa
0xBRL <megustacrypto@gmail.com>
2022-10-26 23:10:29 -0300

    chore: set the project infos

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,3 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
@@ -7,51 +6,33 @@ import "@nomicfoundation/hardhat-chai-matchers";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types/config";
+
 import * as dotenv from "dotenv";
 
 import "./tasks/deploy";
-import { NetworkUserConfig } from "hardhat/types/config";
 
 dotenv.config();
 
-const bscTestnet: NetworkUserConfig = {
+const BSC_TESTNET: NetworkUserConfig = {
   url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
   chainId: 97,
-  accounts: [process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY!],
+  accounts: [process.env.TEST_ACCOUNT_PRIVATE_KEY!],
 };
 
-const bscMainnet: NetworkUserConfig = {
+const BSC_MAINNET: NetworkUserConfig = {
   url: "https://bsc-dataseed.binance.org/",
   chainId: 56,
-  accounts: [process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY!],
+  accounts: [process.env.TEST_ACCOUNT_PRIVATE_KEY!],
 };
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    bscTestnet: bscTestnet,
-    bscMainnet: bscMainnet,
-    hardhat: {
-      mining: {
-        auto: true,
-        interval: 1000,
-      },
-      allowUnlimitedContractSize: true,
-    },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY !== undefined
-          ? [process.env.TEST_ETH_ACCOUNT_PRIVATE_KEY]
-          : [],
-    },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    hardhat: {},
+    bscTestnet: BSC_TESTNET,
+    bscMainnet: BSC_MAINNET,
   },
   paths: {
     sources: "./contracts",
